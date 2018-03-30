9f0e278f05b3e9ce5c65e76512d9cb0913b3ed99
Jean-Yves Le Cabellec <jy.lecabellec@gmail.com>
2018-03-31 01:21:13 +0200

    ”T-Shirt Info” section of the form:
    For the T-Shirt color menu, only display the color options that match the design selected in the "Design" menu.
    If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
    If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."

--- js/form.js
+++ js/form.js
@@ -2,6 +2,19 @@ const nameInput = document.getElementById('name');
 const basicInfoFieldset = document.getElementById('basic-info');
 const selectTitle =  document.getElementById('title');
 const otherTitleInput = document.getElementById('other-title');
+const selectDesign = document.getElementById('design');
+const colorOption = document.querySelectorAll('#color option');
+const punsOptionArray = [];
+const heartOptionArray = [];
+
+for(var i = 0; i < colorOption.length; i += 1) {
+    if(colorOption[i].textContent.includes('JS Puns')) {
+        punsOptionArray.push(colorOption[i]);
+    } else if (colorOption[i].textContent.includes('I ♥ JS')) {
+        heartOptionArray.push(colorOption[i]);
+    }
+}
+
 
 // On document fully loaded do what's below
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -10,8 +23,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 // Check change on title select input
 selectTitle.addEventListener('change', (e) => {
-    const selectedOption = e.target.selectedOptions[0].value;
-    
+    const selectedOption = e.target.selectedOptions[0].value;    
     // Detect if selected is other & display text field with id "other-title" and placeholder "Your Job Role"
     if(selectedOption === 'other') {
         otherTitleInput.className = '';
@@ -19,6 +31,36 @@ selectTitle.addEventListener('change', (e) => {
         otherTitleInput.className = 'is-hidden';
     }
 });
+
+// Select color options
+selectDesign.addEventListener('click', (e) => {
+    const selectedOption = e.target.selectedOptions[0].value;  
+    
+    // If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
+    if(selectedOption === 'js puns') {
+        for(var i = 0; i < punsOptionArray.length; i += 1) {
+            punsOptionArray[i].style.display = 'block';
+        }
+    } else {
+        for(var i = 0; i < punsOptionArray.length; i += 1) {
+            punsOptionArray[i].style.display = 'none';
+        }
+    }
+
+    // If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."
+    if(selectedOption === 'heart js') {
+        for(var i = 0; i < heartOptionArray.length; i += 1) {
+            heartOptionArray[i].style.display = 'block';
+        }
+    } else {
+        for(var i = 0; i < heartOptionArray.length; i += 1) {
+            heartOptionArray[i].style.display = 'none';
+        }
+    }
+});
+
+
+
 
 
 
