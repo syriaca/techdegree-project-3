37a645461213ceada33ab0cdd662570eb3759c6c
Jean-Yves Le Cabellec <jy.lecabellec@gmail.com>
2018-03-30 19:01:59 +0200

    ”Job Role” section of the form
    Detect if selected is other & display text field with id "other-title" and placeholder "Your Job Role"

--- js/form.js
+++ js/form.js
@@ -1,5 +1,25 @@
-var nameInput = document.getElementById('name');
+const nameInput = document.getElementById('name');
+const basicInfoFieldset = document.getElementById('basic-info');
+const selectTitle =  document.getElementById('title');
+const otherTitleInput = document.getElementById('other-title');
 
+// On document fully loaded do what's below
 document.addEventListener("DOMContentLoaded", function(event) {
     nameInput.focus();
 })
+
+// Check change on title select input
+selectTitle.addEventListener('change', (e) => {
+    const selectedOption = e.target.selectedOptions[0].value;
+    
+    // Detect if selected is other & display text field with id "other-title" and placeholder "Your Job Role"
+    if(selectedOption === 'other') {
+        otherTitleInput.className = '';
+    } else {
+        otherTitleInput.className = 'is-hidden';
+    }
+});
+
+
+
+
